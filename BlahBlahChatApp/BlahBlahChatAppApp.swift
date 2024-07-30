863e8c80f7168b2d44f06800588dc6ab2db00c2d
Andrew <andrewhardin@onenex.co>
2024-07-30 23:45:12 +0630

    Initial Commit

---
+++ BlahBlahChatApp/BlahBlahChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlahBlahChatAppApp.swift
+//  BlahBlahChatApp
+//
+//  Created by Andrew Hardin on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BlahBlahChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlahBlahChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlahBlahChatApp
+//
+//  Created by Andrew Hardin on 7/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
